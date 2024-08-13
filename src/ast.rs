ef4fe9f1b032a36bd1b507d24bb0d61bd162c351
trifledmatter <ethan@gradison.ca>
2024-08-13 18:37:58 -0300

    feat: update ast with more properties

--- src/ast.rs
+++ src/ast.rs
@@ -1,26 +1,40 @@
-#[derive(Debug, PartialEq, Clone)]
+// src/ast.rs
+
+#[derive(Debug, Clone)]
 pub enum Expr {
-    Literal(Literal),
     Identifier(String),
+    Literal(Literal),
     Binary(Box<Expr>, Operator, Box<Expr>),
-    Group(Vec<Expr>),
-    Call(Box<Expr>, Vec<Expr>),
+    Unary(Operator, Box<Expr>),
+    Group(Box<Expr>),
+    FunctionCall(String, Vec<Expr>),
+    Index(Box<Expr>, Box<Expr>), // array or map indexing
+    PropertyAccess(Box<Expr>, String), // accessing properties of objects/structs
+    Assignment(Box<Expr>, Box<Expr>), // assignment expression
+    Ternary(Box<Expr>, Box<Expr>, Box<Expr>), // ternary condition (cond ? true_expr : false_expr)
+    Lambda(Vec<String>, Box<Expr>), // lambda expressions
+    Array(Vec<Expr>), // array literals
+    Map(Vec<(Expr, Expr)>), // map (key-value) literals
+    Cast(Box<Expr>, String), // type casting
 }
 
-#[derive(Debug, PartialEq, Clone)]
+#[derive(Debug, Clone)]
 pub enum Literal {
     Number(f64),
     String(String),
     Boolean(bool),
-    Nil,
+    Null, // for null values
+    Array(Vec<Expr>), // array literals
+    Map(Vec<(Expr, Expr)>), // map (key-value) literals
 }
 
-#[derive(Debug, PartialEq, Clone)]
+#[derive(Debug, Clone)]
 pub enum Operator {
     Add,
     Subtract,
     Multiply,
     Divide,
+    Modulo,
     Equal,
     NotEqual,
     Less,
@@ -30,19 +44,30 @@ pub enum Operator {
     And,
     Or,
     Not,
+    BitwiseAnd,
+    BitwiseOr,
+    BitwiseXor,
+    BitwiseNot,
+    LeftShift,
+    RightShift,
 }
 
-#[derive(Debug, PartialEq, Clone)]
+#[derive(Debug, Clone)]
 pub enum Stmt {
     Expression(Expr),
     Print(Expr),
-    Var(String, Option<Expr>),
-    Block(Vec<Stmt>),
-    If(Expr, Box<Stmt>, Option<Box<Stmt>>),
-    While(Expr, Box<Stmt>),
-    // Function(String, Vec<String>, Box<Stmt>),
-    FunctionDeclaration(String, Vec<String>, Box<Stmt>),
-    FunctionCall(Box<Expr>, Vec<Expr>),
     VariableDeclaration(String, Option<Expr>),
-    FunctionParameters(Vec<String>),
+    FunctionDeclaration(String, Vec<String>, Vec<Stmt>),
+    If(Expr, Vec<Stmt>, Option<Vec<Stmt>>),
+    While(Expr, Vec<Stmt>),
+    For(String, Expr, Expr, Vec<Stmt>),
+    ForEach(String, Expr, Vec<Stmt>),
+    Block(Vec<Stmt>),
+    Return(Option<Expr>),
+    Break,
+    Continue,
+    Import(String),
+    StructDeclaration(String, Vec<(String, String)>),
+    ClassDeclaration(String, Vec<(String, String)>, Vec<Stmt>),
+    MethodDeclaration(String, Vec<String>, Vec<Stmt>), // for methods within a class
 }
